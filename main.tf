9df7871cdbf72406aa3967f4143e21d6c6558f0f
SHewitt95 <hewitt.sherman@gmail.com>
2021-06-06 21:01:27 -0400

    static website

--- main.tf
+++ main.tf
@@ -26,3 +26,16 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "static" {
+  bucket = "bucket-by-terraform-static"
+  acl    = "public-read"
+
+  website {
+    index_document: "index.html"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
